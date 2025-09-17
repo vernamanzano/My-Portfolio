8dea2495484a2232fc239e4247482bf9f38f42da
Verna Manzano <manzano.vernalyn@gmail.com>
2025-09-18 04:27:13 +0800

    updated work info

--- src/components/DevWork.vue
+++ src/components/DevWork.vue
@@ -1,140 +1,179 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-    <div id="dev-work-section" class="pt-20 space-y-4 text-dark-charcoal">
-        <div class="flex items-end justify-between mb-8">
-            <h2 class="text-5xl md:text-8xl font-semibold text-dark-charcoal">
-                Dev Work
-            </h2>
-            <p class="bg-dark-charcoal text-white rounded-full px-4 py-2">
-                2023
-            </p>
-        </div>
-
-        <div>
-            <div class="space-y-4">
-                <a href="https://aceada.com/" class="text-3xl md:text-5xl"> AceADA </a>
-                <ul class="flex flex-wrap gap-2">
-                    <li class="skill">
-                        HTML
-                    </li>
-                    <li class="skill">
-                        CSS
-                    </li>
-                    <li class="skill">
-                        Tailwind CSS
-                    </li>
-                    <li class="skill">
-                        JS
-                    </li>
-                    <li class="skill">
-                        Vue.js
-                    </li>
-                    <li class="skill">
-                        PHP Laravel
-                    </li>
-                    <li class="skill">
-                        Figma
-                    </li>
-                </ul>
-                <ul class="list-disc pl-5 lg:w-1/2 space-y-2">
-                    <li>
-                        Enhanced an inclusive accessibility application, enabling dynamic website adjustments to cater to diverse user needs, including enhanced focus, voice commands, screen reader, and other features.
-                    </li>
-                    <li>
-                        Developed user-friendly web pages for both administrators and clients, for app management and integration into their websites.
-                    </li>
-                </ul>
-            </div>
-
-            <div>
-                <img class="mx-auto my-8" src="../assets/images/aceada-3d-mockup.png">
-            </div>
-        </div>
-
-        <div class="py-8">
-            <div class="space-y-4">
-                <a href="https://easyhire.app/" target="_blank" class="text-3xl md:text-5xl"> EasyHire </a>
-                <ul class="flex flex-wrap gap-2">
-                    <li class="skill">
-                        HTML
-                    </li>
-                    <li class="skill">
-                        CSS
-                    </li>
-                    <li class="skill">
-                        JS
-                    </li>
-                    <li class="skill">
-                        Bootstrap
-                    </li>
-                    <li class="skill">
-                        PHP Laravel
-                    </li>
-                    <li class="skill">
-                        Figma
-                    </li>
-                </ul>
-                <ul class="list-disc pl-5 lg:w-1/2 space-y-2">
-                    <li>
-                        Designed and developed an employment website for candidates and employers
-                    </li>
-                    <li>
-                        Implemented a test library for candidate assessment, aiding employers in evaluating job skills, personality traits, and compatibility.
-                    </li>
-                </ul>
-            </div>
-
-            <div>
-                <img class="mx-auto my-8" src="../assets/images/easyhire-mockup.png">
-            </div>
-        </div>
-
-        <div>
-            <div class="space-y-4">
-                <a href="https://restock.legacyseller.com/" class="text-3xl md:text-5xl"> Legacy Restock </a>
-                <ul class="flex flex-wrap gap-2">
-                    <li class="skill">
-                        HTML
-                    </li>
-                    <li class="skill">
-                        CSS
-                    </li>
-                    <li class="skill">
-                        JS
-                    </li>
-                    <li class="skill">
-                        Bootstrap
-                    </li>
-                    <li class="skill">
-                        PHP Laravel
-                    </li>
-                    <li class="skill">
-                        Figma
-                    </li>
-                </ul>
-                <ul class="list-disc pl-5 lg:w-1/2 space-y-2">
-                    <li>
-                        Developed webpages serving sellers to efficiently manage and replenish their Amazon inventory
-                    </li>
-                </ul>
-            </div>
-
-            <div>
-                <img class="mx-auto w-2/3 my-8" src="../assets/images/legacy-restock-mockup.png">
-            </div>
-        </div>
+  <div id="dev-work-section" class="pt-20 space-y-4 text-dark-charcoal">
+    <div class="flex items-end justify-between mb-8">
+      <h2 class="text-5xl md:text-8xl font-semibold text-dark-charcoal">
+        Dev Work
+      </h2>
+      <p class="bg-dark-charcoal text-white rounded-full px-4 py-2">
+        2023-2025
+      </p>
     </div>
 
+    <div>
+      <div class="space-y-4">
+        <span class="text-3xl md:text-5xl"> Auditing System </span>
+        <ul class="flex flex-wrap gap-2">
+          <li class="skill">HTML</li>
+          <li class="skill">CSS</li>
+          <li class="skill">JS</li>
+          <li class="skill">PHP</li>
+          <li class="skill">SQL</li>
+          <li class="skill">Vue.js</li>
+          <li class="skill">Bootstrap</li>
+          <li class="skill">Laravel</li>
+          <li class="skill">Git</li>
+          <li class="skill">Figma</li>
+        </ul>
+        <ul class="list-disc pl-5 lg:w-1/2 space-y-2">
+          <li>
+            Designed and developed a system that replaced Excel workflows,
+            enabling company auditors to process reimbursements more efficiently
+            and recover thousands of dollars in lost inventory
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <br />
+
+    <div>
+      <div class="space-y-4">
+        <span class="text-3xl md:text-5xl"> Accounting System </span>
+        <ul class="flex flex-wrap gap-2">
+          <li class="skill">HTML</li>
+          <li class="skill">CSS</li>
+          <li class="skill">JS</li>
+          <li class="skill">PHP</li>
+          <li class="skill">SQL</li>
+          <li class="skill">Vue.js</li>
+          <li class="skill">Bootstrap</li>
+          <li class="skill">Laravel</li>
+          <li class="skill">Git</li>
+          <li class="skill">Figma</li>
+        </ul>
+        <ul class="list-disc pl-5 lg:w-1/2 space-y-2">
+          <li>
+            Contributed to a custom accounting system by developing frontend
+            features with backend APIs
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <br />
+
+    <div>
+      <div class="space-y-4">
+        <span href="https://aceada.com/" class="text-3xl md:text-5xl">
+          AceADA
+        </span>
+        <ul class="flex flex-wrap gap-2">
+          <li class="skill">HTML</li>
+          <li class="skill">CSS</li>
+          <li class="skill">JS</li>
+          <li class="skill">PHP</li>
+          <li class="skill">Vue.js</li>
+          <li class="skill">Tailwind</li>
+          <li class="skill">Laravel</li>
+          <li class="skill">Git</li>
+          <li class="skill">Figma</li>
+        </ul>
+        <ul class="list-disc pl-5 lg:w-1/2 space-y-2">
+          <li>
+            Enhanced an inclusive accessibility application, enabling dynamic
+            website adjustments to cater to diverse user needs, including
+            enhanced focus, voice commands, screen reader, and other features.
+          </li>
+          <li>
+            Developed user-friendly web pages for both administrators and
+            clients, for app management and integration into their websites.
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <img class="mx-auto my-8" src="../assets/images/aceada-3d-mockup.png" />
+      </div>
+    </div>
+
+    <div class="py-8">
+      <div class="space-y-4">
+        <span
+          href="https://easyhire.app/"
+          target="_blank"
+          class="text-3xl md:text-5xl"
+        >
+          EasyHire
+        </span>
+        <ul class="flex flex-wrap gap-2">
+          <li class="skill">HTML</li>
+          <li class="skill">CSS</li>
+          <li class="skill">JS</li>
+          <li class="skill">PHP</li>
+          <li class="skill">Vue.js</li>
+          <li class="skill">Bootstrap</li>
+          <li class="skill">Laravel</li>
+          <li class="skill">Git</li>
+          <li class="skill">Figma</li>
+        </ul>
+        <ul class="list-disc pl-5 lg:w-1/2 space-y-2">
+          <li>
+            Designed and developed an employment website for candidates and
+            employers
+          </li>
+          <li>
+            Implemented a test library for candidate assessment, aiding
+            employers in evaluating job skills, personality traits, and
+            compatibility.
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <img class="mx-auto my-8" src="../assets/images/easyhire-mockup.png" />
+      </div>
+    </div>
+
+    <div>
+      <div class="space-y-4">
+        <span class="text-3xl md:text-5xl"> Legacy Restock </span>
+        <ul class="flex flex-wrap gap-2">
+          <li class="skill">HTML</li>
+          <li class="skill">CSS</li>
+          <li class="skill">JS</li>
+          <li class="skill">PHP</li>
+          <li class="skill">Vue.js</li>
+          <li class="skill">Tailwind</li>
+          <li class="skill">Laravel</li>
+          <li class="skill">Git</li>
+          <li class="skill">Figma</li>
+        </ul>
+        <ul class="list-disc pl-5 lg:w-1/2 space-y-2">
+          <li>
+            Developed webpages serving sellers to efficiently manage and
+            replenish their Amazon inventory
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <img
+          class="mx-auto w-2/3 my-8"
+          src="../assets/images/legacy-restock-mockup.png"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
-    .skill {
-        display: inline;
-        background: #F1F5F9;
-        padding: 4px 8px;
-        border-radius: 8px;
-        color: #334155;
-    }
+.skill {
+  display: inline;
+  background: #f1f5f9;
+  padding: 4px 8px;
+  border-radius: 8px;
+  color: #334155;
+}
 </style>
