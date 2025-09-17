47fcd484254e03cb723738dec5650b184665b9d2
Verna Manzano <manzano.vernalyn@gmail.com>
2025-09-18 04:36:09 +0800

    updated the about section

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,145 +1,128 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-    <main class="font-Jakarta container my-16 px-4 text-dark-charcoal">
-        <header class="text-6xl lg:text-8xl font-bold">
-            I can learn anything. <br class="block md:hidden"> I can know anything. <br class="block md:hidden"> I can be anything.
-        </header>
+  <main class="font-Jakarta container my-16 px-4 text-dark-charcoal">
+    <header class="text-6xl lg:text-8xl font-bold">
+      I can learn anything. <br class="block md:hidden" />
+      I can know anything. <br class="block md:hidden" />
+      I can be anything.
+    </header>
 
-        <div class="grid grid-none lg:grid-cols-3 gap-8 xl:gap-32 my-20 text-xl">
-            <div class="space-y-8">
-                <p>
-                    Hi, my name is Verna and that quote above is what keeps me going.
-                </p>
+    <div class="grid grid-none lg:grid-cols-3 gap-8 xl:gap-32 my-20 text-xl">
+      <div class="space-y-8">
+        <p>Hi, my name is Verna and that quote above is what keeps me going.</p>
 
-                <div>
-                    <h2 class="font-semibold">
-                        Embracing the Unexpected
-                    </h2>
-                    <div class="space-y-8">
-                        <p>
-                            In this ever-evolving digital landscape, I've come to appreciate that adaptability and a thirst for learning are my strongest assets.
-                            
-                        </p>
-                        <p>
-                            After college, I decided to hone my technical skills by studying and by the help of my mentor in the IT field.
-                        </p>
-                        
-                    </div>
-                </div>
+        <div>
+          <h2 class="font-semibold">Embracing the Unexpected</h2>
+          <div class="space-y-8">
+            <p>
+              In this ever-evolving digital landscape, I've come to appreciate
+              that adaptability and a thirst for learning are my strongest
+              assets.
+            </p>
+            <p>
+              After college, I decided to hone my technical skills by studying
+              and by the help of my mentor in the IT field.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p>
+          If you're wondering how I fit into the world of design and web
+          development, the answer is simple:
 
+          <br />
+          <br />
 
-            </div>
-            <div>
-                <p>
-                    Still, my degree in Broadcasting has taught me the art of effective communication, and now I'm using those skills to communicate through code, layout, and design elements.
-                    <br>
-                    <br>
-                    
-                    So, if you're wondering how I fit into the world of design and web development, the answer is simple:
-
-                    <br>
-                    <br>
-
-                    I'm here to bridge the gap between storytelling and technology.
-                </p>
-            </div>
-            <div>
-                <h2 class="font-semibold">
-                    Let's connect!
-                </h2>
-
-                <div>
-                    <p>
-                        If you're looking for a creative collaborator or just want to swap stories, feel free to reach out.
-                        
-                    </p>
-                    <br>
-                    <p>
-                        Your digital dreamweaver,
-                        <img class="my-4" src="../assets/images/signature.png">
-                    </p>
-                    
-                </div>
-
-                <div class="flex justify-start gap-2 pt-8 pl-1">
-                    <a href="https://mail.google.com/mail/u/1/?view=cm&fs=1&to=manzano.vernalyn@gmail.com&tf=1" target="_blank">
-                      <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M16 0C24.8366 0 32 7.16344 32 16C32 24.8366 24.8366 32 16 32C7.16344 32 0 24.8366 0 16C0 7.16344 7.16344 0 16 0ZM19.2078 12.3782C18.3787 11.5855 17.3241 11.1818 16.1496 11.1818C14.066 11.1818 12.3026 12.589 11.6735 14.48C11.5135 14.96 11.4226 15.4727 11.4226 16C11.4226 16.5272 11.5132 17.04 11.6732 17.52C12.3023 19.4109 14.066 20.8181 16.1496 20.8181C17.226 20.8181 18.1426 20.5346 18.859 20.0546C19.7063 19.4873 20.2699 18.64 20.4554 17.64H16.1499V14.5455H23.6845C23.779 15.0691 23.8299 15.6146 23.8299 16.1818C23.8299 18.6182 22.9572 20.6691 21.4445 22.0619C20.1208 23.2837 18.3096 24 16.1496 24C13.0223 24 10.3169 22.2072 9.00049 19.5927C8.45867 18.5127 8.1499 17.2909 8.1499 16C8.1499 14.709 8.45899 13.4872 9.00081 12.4072C10.3172 9.79268 13.0223 8 16.1496 8C18.306 8 20.1169 8.79273 21.5023 10.0836L19.2078 12.3782Z" fill="#31352E"/>
-                      </svg>
-                    </a>
-                    <a href="https://www.linkedin.com/in/vernalynmanzano/" target="_blank">
-                      <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M0 16C0 7.16344 7.16344 0 16 0C24.8366 0 32 7.16344 32 16C32 24.8366 24.8366 32 16 32C7.16344 32 0 24.8366 0 16ZM8.2 13.3V24H11.6V13.3H8.2ZM8 9.9C8 11 8.8 11.8 9.9 11.8C11 11.8 11.8 11 11.8 9.9C11.8 8.8 11 8 9.9 8C8.9 8 8 8.8 8 9.9ZM20.6 24H23.8V17.4C23.8 14.1 21.8 13 19.9 13C18.2 13 17 14.1 16.7 14.8V13.3H13.5V24H16.9V18.3C16.9 16.8 17.9 16 18.9 16C19.9 16 20.6 16.5 20.6 18.2V24Z" fill="#31352E"/>
-                      </svg>
-                    </a>
-                  </div>
-            </div>
+          I'm here to bridge the gap between storytelling and technology.
+        </p>
+        <br />
+        <h2 class="font-semibold">Let's connect!</h2>
+      </div>
+      <div>
+        <div>
+          <p>
+            If you're looking for a creative collaborator or just want to swap
+            stories, feel free to reach out.
+          </p>
+          <br />
+          <p>
+            Your digital dreamweaver,
+            <img class="my-4" src="../assets/images/signature.png" />
+          </p>
         </div>
 
-        <div class="xl:w-2/3">
-            <ul class="flex flex-wrap text-sm md:text-regular">
-                <li class="skill">
-                    HTML
-                </li>
-                <li class="skill">
-                    CSS
-                </li>
-                <li class="skill">
-                    Tailwind CSS
-                </li>
-                <li class="skill">
-                    Vue.js
-                </li>
-                <li class="skill">
-                    PHP Laravel
-                </li>
-                <li class="skill">
-                    Laragon
-                </li>
-                <li class="skill">
-                    Git
-                </li>
-                <li class="skill">
-                    VS Code
-                </li>
-                <li class="skill">
-                    Figma
-                </li>
-                <li class="skill">
-                    Adobe Photoshop
-                </li>
-                <li class="skill">
-                    Adobe Illustrator
-                </li>
-                <li class="skill">
-                    Adobe InDesign
-                </li>
-                <li class="skill">
-                    Adobe Premiere Pro
-                </li>
-                <li class="skill">
-                    Canva
-                </li>
-                <li class="skill">
-                    Adobe Audition
-                </li>
-                <li class="skill">
-                    OBS Studio
-                </li>
-                <li class="skill">
-                    Microsoft Office
-                </li>
-            </ul>
+        <div class="flex justify-start gap-2 pt-8 pl-1">
+          <a
+            href="https://mail.google.com/mail/u/1/?view=cm&fs=1&to=manzano.vernalyn@gmail.com&tf=1"
+            target="_blank"
+          >
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M16 0C24.8366 0 32 7.16344 32 16C32 24.8366 24.8366 32 16 32C7.16344 32 0 24.8366 0 16C0 7.16344 7.16344 0 16 0ZM19.2078 12.3782C18.3787 11.5855 17.3241 11.1818 16.1496 11.1818C14.066 11.1818 12.3026 12.589 11.6735 14.48C11.5135 14.96 11.4226 15.4727 11.4226 16C11.4226 16.5272 11.5132 17.04 11.6732 17.52C12.3023 19.4109 14.066 20.8181 16.1496 20.8181C17.226 20.8181 18.1426 20.5346 18.859 20.0546C19.7063 19.4873 20.2699 18.64 20.4554 17.64H16.1499V14.5455H23.6845C23.779 15.0691 23.8299 15.6146 23.8299 16.1818C23.8299 18.6182 22.9572 20.6691 21.4445 22.0619C20.1208 23.2837 18.3096 24 16.1496 24C13.0223 24 10.3169 22.2072 9.00049 19.5927C8.45867 18.5127 8.1499 17.2909 8.1499 16C8.1499 14.709 8.45899 13.4872 9.00081 12.4072C10.3172 9.79268 13.0223 8 16.1496 8C18.306 8 20.1169 8.79273 21.5023 10.0836L19.2078 12.3782Z"
+                fill="#31352E"
+              />
+            </svg>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/vernalynmanzano/"
+            target="_blank"
+          >
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M0 16C0 7.16344 7.16344 0 16 0C24.8366 0 32 7.16344 32 16C32 24.8366 24.8366 32 16 32C7.16344 32 0 24.8366 0 16ZM8.2 13.3V24H11.6V13.3H8.2ZM8 9.9C8 11 8.8 11.8 9.9 11.8C11 11.8 11.8 11 11.8 9.9C11.8 8.8 11 8 9.9 8C8.9 8 8 8.8 8 9.9ZM20.6 24H23.8V17.4C23.8 14.1 21.8 13 19.9 13C18.2 13 17 14.1 16.7 14.8V13.3H13.5V24H16.9V18.3C16.9 16.8 17.9 16 18.9 16C19.9 16 20.6 16.5 20.6 18.2V24Z"
+                fill="#31352E"
+              />
+            </svg>
+          </a>
         </div>
-    </main>
+      </div>
+    </div>
+
+    <div class="xl:w-2/3">
+      <ul class="flex flex-wrap text-sm md:text-regular">
+        <li class="skill">HTML</li>
+        <li class="skill">CSS</li>
+        <li class="skill">Tailwind CSS</li>
+        <li class="skill">Vue.js</li>
+        <li class="skill">PHP Laravel</li>
+        <li class="skill">Laragon</li>
+        <li class="skill">Git</li>
+        <li class="skill">VS Code</li>
+        <li class="skill">Figma</li>
+        <li class="skill">Adobe Photoshop</li>
+        <li class="skill">Adobe Illustrator</li>
+        <li class="skill">Adobe InDesign</li>
+        <li class="skill">Adobe Premiere Pro</li>
+        <li class="skill">Canva</li>
+        <li class="skill">Adobe Audition</li>
+        <li class="skill">OBS Studio</li>
+        <li class="skill">Microsoft Office</li>
+      </ul>
+    </div>
+  </main>
 </template>
 
 <style scoped>
-
 .skill {
-    margin-top: 10px;
-    margin-right: 10px;
+  margin-top: 10px;
+  margin-right: 10px;
 }
 </style>
